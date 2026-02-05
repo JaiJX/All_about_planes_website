d3493e5c1e0b08d0bc3561ed00ae47696a516b18
JaiJX <132177135+JaiJX@users.noreply.github.com>
2026-02-05 06:37:41 +0000

    V13 Formated for Hosting

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("Hello Jaison, Your Script Works.")
+
+
+function dropDown() {
+    const dropcontent = document.querySelector('.dropitems');
+    dropcontent.classList.toggle('show');
+
+    //Closing menu
+    window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropitems");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}}
+console.log("Script Works")
